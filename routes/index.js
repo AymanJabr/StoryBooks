287ceb15ab97911364230d8fba0d4223686bae69
ayman <aymanjaber2012@gmail.com>
2021-05-02 15:17:02 +0300

    Add authentication

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 // For all main routes, routes that are not followed by anything, eg: /auth/
 const express = require('express')
 const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 
 //@desc Login/Landing Page
 //@route GET /
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
     res.render("login", {
         layout: 'login'
     })
@@ -13,7 +14,7 @@ router.get('/', (req, res) => {
 
 //@desc Dashboard
 //@route GET /dashboard
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuth, (req, res) => {
     res.render("dashboard")
 })
 
