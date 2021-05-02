833e98001d076707597c472485fbe4e3676aa156
ayman <aymanjaber2012@gmail.com>
2021-05-02 11:45:45 +0300

    added the db.js file in order to connect to the Mongoose database

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const connectDB = async() => {
+    try {
+        //Extra options are just used to stop some warning messages
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+
+        console.log(`MongoDB connected: ${conn.connection.host}`)
+    } catch (err) {
+        console.error(err)
+        process.exit(1)        
+    }
+}
+
+
+module.exports = connectDB
