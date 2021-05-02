0f0cfdd47bf7baba46c88a06a445afbfa73767ba
ayman <aymanjaber2012@gmail.com>
2021-05-02 14:36:50 +0300

    Finished authentication with google

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+// For authentication
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+
+//@desc Authenticate with Google
+//@route GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile']}))
+
+//@desc Google Auth Callback
+//@route GET /auth/google/callback
+router.get('/google/callback', passport.authenticate('google', { 
+    failureRedirect: '/'
+}), (req,res) => {
+    res.redirect('/dashboard')
+})
+
+
+
+module.exports = router
